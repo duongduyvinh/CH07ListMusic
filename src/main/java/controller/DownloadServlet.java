079260f2c0aeae349878d724c537d8adecd16998
duongduyvinh <duongduyvinh@users.noreply.github.com>
2026-09-15 01:17:45 +0700

    Fix Tomcat startup 404 conflict by removing @WebServlet annotation

--- src/main/java/controller/DownloadServlet.java
+++ src/main/java/controller/DownloadServlet.java
@@ -3,13 +3,11 @@ package controller;
 import model.User;
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/download")
 public class DownloadServlet extends HttpServlet {
 
     @Override
